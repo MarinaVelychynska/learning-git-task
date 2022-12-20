6f6259f7e766766686f6185581236e8272eff103
Maryna Velychynska <marien5@ukr.net>
2022-12-20 22:08:55 +0100

    Init project.py

--- Task_to_commit.py
+++ Task_to_commit.py
@@ -3,4 +3,5 @@ my_shopping_dict = {"пекарня": ["хліб", "булочки", "пончи
 for k, v in my_shopping_dict.items():
   products = [product.title() for product in v]
   print(f"Я йду до магазину {k.title()}, і купую там такі товари {products}")
-  
+  sum_dict_values = sum((len(v) for v in my_shopping_dict.values()))
+print(f"Я разом купую {sum_dict_values} товарів.")
