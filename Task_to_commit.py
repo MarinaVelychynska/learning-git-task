c15f3e920886bad44fe3d0aa1466bb9d69c20e6e
Maryna Velychynska <marien5@ukr.net>
2022-12-20 22:06:09 +0100

    Init project.py

--- Task_to_commit.py
+++ Task_to_commit.py
@@ -1,2 +1,6 @@
 print("Список покупок")
 my_shopping_dict = {"пекарня": ["хліб", "булочки", "пончик"], "овочевий": ["морква", "селера", "рукола"]}
+for k, v in my_shopping_dict.items():
+  products = [product.title() for product in v]
+  print(f"Я йду до магазину {k.title()}, і купую там такі товари {products}")
+  
