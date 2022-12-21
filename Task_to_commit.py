8afa5218c437082b5a6689d846ba8b177b04d5dc
Maryna Velychynska <marien5@ukr.net>
2022-12-21 03:44:54 +0100

    Comit_reserve

--- Task_to_commit.py
+++ Task_to_commit.py
@@ -1,4 +1,7 @@
+print("Hello mentor")
+
 print("Список покупок")
+
 my_shopping_dict = {"пекарня": ["хліб", "булочки", "пончик"], "овочевий": ["морква", "селера", "рукола"]}
 for k, v in my_shopping_dict.items():
   products = [product.title() for product in v]
